180faf77995e1f2a981c9e76dccc2e367cb536ba
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-11 17:17:18 +0300

    db migrations applied

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,8 +1,19 @@
-﻿namespace DataAccessLayer.Concrete
-{
-    public class Context
-    {
+﻿using System.Data.Entity;
+using EntityLayer.Concrete;
 
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbMvcSozluk")
+        { }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
 
     }
 }
