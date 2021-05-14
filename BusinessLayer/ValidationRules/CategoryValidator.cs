935b04a56e269fa45d533de49da0e47282e5f958
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-14 16:21:42 +0300

    L32 - html helper form, validation messages on view

--- BusinessLayer/ValidationRules/CategoryValidator.cs
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -7,7 +7,7 @@ namespace BusinessLayer.ValidationRules
     {
         public CategoryValidator()
         {
-            RuleFor(c => c.CategoryName).NotNull().NotEmpty().WithMessage("Kategori adı boş olamaz");
+            RuleFor(c => c.CategoryName).NotEmpty().WithMessage("Kategori adı boş olamaz");
             RuleFor(c => c.CategoryDescription).NotNull().NotEmpty().WithMessage("Kategori açıklaması boş olamaz");
             RuleFor(c => c.CategoryName).MinimumLength(3).WithMessage("Kategori adı 3 karaktereden az olamaz");
             RuleFor(c => c.CategoryName).MaximumLength(20).WithMessage("Kategori adı 20 karaktereden fazla olamaz");
