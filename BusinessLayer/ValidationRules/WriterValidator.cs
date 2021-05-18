8cf559e4358b3684c59e7580f120ccf656d55e25
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-19 02:26:21 +0300

    add writertitle

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -10,6 +10,7 @@ namespace BusinessLayer.ValidationRules
             RuleFor(w => w.WriterName).NotEmpty().WithMessage("Yazar adı boş olamaz");
             RuleFor(w => w.WriterName).MinimumLength(2).WithMessage("Yazar adı 2 karaktereden az olamaz");
             RuleFor(w => w.WriterSurname).NotEmpty().WithMessage("Yazar soyadı boş olamaz");
+            RuleFor(w => w.WriterTitle).MinimumLength(2).WithMessage("Yazar Ünvanı 2 karaktereden az olamaz");
             RuleFor(w => w.WriterSurname).MinimumLength(2).WithMessage("Yazar soyadı 2 karaktereden az olamaz");
 
 
