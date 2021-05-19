0b6e89e9234fc1f31baf3b16eb5d564cec18eeb6
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-19 03:04:22 +0300

    added and listed headings

---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        private HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Index()
+        {
+            var headingValues = hm.GetList();
+            return View(headingValues);
+        }
+    }
+}
