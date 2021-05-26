3169de5d4229d693191aca50bd94fb13586d5487
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-26 03:47:12 +0300

    added and implemented about

---
+++ BusinessLayer/Abstract/IAboutService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IAboutService
+    {
+        List<About> GetList();
+        About GetByID(int id);
+        void AboutAdd(About about);
+        void AboutDelete(About about);
+        void AboutUpdate(About about);
+    }
+}
